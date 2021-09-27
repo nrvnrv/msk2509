cd6ccd27f0f77a49bd5b8f522b6f124a022e7ac1
nrvnrv <57039705+nrvnrv@users.noreply.github.com>
2021-09-27 10:02:45 +0300

    deleted extra modules

--- coco/run.py
+++ coco/run.py
@@ -1,25 +1,12 @@
 import time
 from modules.Detect import Detect
-
-# added
-import uvicorn, asyncio, cv2
-from vidgear.gears.asyncio import WebGear
-from vidgear.gears.asyncio.helper import reducer
-
-web = WebGear(logging=True)#
-
+from cv2 import waitKey
 start_time = time.time()
 detect = Detect(256).start()
-
-web.config["generator"] = detect.my_frame_producer#
-uvicorn.run(web(), host="localhost", port=8000)#
-
-
-
-
 print('Init finished')
 print("--- took %s seconds ---" % (time.time() - start_time))
-print()
-web.shutdown()#
 detect.enable()
-time.sleep(1000)
+
+while waitKey(27) != 27:
+    time.sleep(1)
+
